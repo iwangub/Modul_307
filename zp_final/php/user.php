10d73261a687117c88c0d3b4115cee54904f4635
Gubwan <iwan.gubler89@gmail.com>
2023-05-19 13:25:24 +0200

    cleanup commens

--- zp_final/php/user.php
+++ zp_final/php/user.php
@@ -1,4 +1,6 @@
 <?php
+// this PHP file is used to check if the username is already taken
+
 if (isset($_POST['username'])) {
 	$string = "user admin dbuser Barbara Claudio Peter Fritz Hans Oliver Kurt";
 	$un = htmlspecialchars($_POST['username']);
